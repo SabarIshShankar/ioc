f8ba85d4fa6f560bef0401bf33a878b8950e0835
sabarIshs19 <66781581+sabarIshs19@users.noreply.github.com>
2021-09-15 21:52:50 +0530

    userprofile 2

--- src/components/UserProfile/UserProfile.tsx
+++ src/components/UserProfile/UserProfile.tsx
@@ -9,3 +9,32 @@ import {changeUsername} from '../../store/message/actions';
 interface IUserProfileState{
     username: string;
 }
+
+export class UserProfile extends React.Component<{translations: IAppContext}>{
+    public state: IUserProfileState = {
+        username: readRecord('username') || '0001'
+    };
+    public render(){
+        const {translations} = this.props;
+        const {username} = this.state;
+        
+        return (
+            <StyledUserProfile>
+                <label htmlFor='username'>{translations.userName}</label>
+                <input type="text" name='username' value={username} onChange={this.handleUserNameChange} />
+            </StyledUserProfile>
+        );
+    }
+
+    private handleUserNameChange= (e: React.FormEvent<HTMLInputElement>) => {
+        this.setState({username: e.currentTarget.value});
+        storeToLocalStorageDebounced('username', e.currentTarget.value);
+        //@ts-ignore
+        this.props.changeUsername(e.currentTarget.value);
+    };
+}
+const mapDispatchToProps = (dispatch: Dispatch<any>) => ({
+    changeUsername: (username: string) => dispatch(changeUsername(username))
+});
+
+export default connect(null, mapDispatchToProps)(UserProfile);
