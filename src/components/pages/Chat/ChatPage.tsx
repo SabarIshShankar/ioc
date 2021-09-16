0cc29935b07293f31c75d1bb4ba59cba14e6fde7
sabarIshs19 <66781581+sabarIshs19@users.noreply.github.com>
2021-09-16 13:45:44 +0530

    settings

---
+++ src/components/pages/Chat/ChatPage.tsx
@@ -0,0 +1,13 @@
+import ChatArea from '../../ChatArea';
+import MessageSender from '../../MessageSender';
+import StyledPageContainer from '../StyledPageContainer';
+import {withTranslations} from '../../../utilities/withTranslations';
+
+const ChatPage = () => (
+    <StyledPageContainer>
+        <ChatArea/>
+        <MessageSender/>
+    </StyledPageContainer>
+);
+
+export default withTranslations(ChatPage);
---
+++ src/components/pages/Settings/SettingsPage.tsx
@@ -0,0 +1,3 @@
+import * as React from 'react';
+import ClockModeSelector from '../../ClockModeSelector';
+import LanguageSelector from '../../LanguageSelector';
