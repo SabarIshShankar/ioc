cc33c22f1d8e1b0e2a1c5f4695db20d1458e3588
sabarIshs19 <66781581+sabarIshs19@users.noreply.github.com>
2021-09-17 11:26:17 +0530

    index ls

---
+++ src/components/LanguageSelector/StyledLanguageSelector.tsx
@@ -0,0 +1,17 @@
+import styled from '../../theme';
+
+const StyledStyledLanguageSelector = styled("div")`
+padding: 14px;
+text-align: left;
+border-bottom: 1px solid ${props => props.theme.primaryDarkColor};
+
+select{
+    color: ${props => props.theme.primaryDarkColor};
+    width: 100%;
+    line-height: 28px;
+    margin-top: 7px;
+    box-sizing: border-box;
+    padding: 7px;
+}`;
+
+export default StyledStyledLanguageSelector
